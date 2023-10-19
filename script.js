7c23cee01f1b8b095b280e1a9cdff619c94f01fc
roopesh-jsp <rupzkumar5@gmail.com>
2023-10-19 12:07:28 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 'use strict';
 
 
+
 document.querySelector('.btn').addEventListener('click',function(){
     let a=document.querySelector('#name').value;
     let b=document.querySelector('#password').value;
     if(!a){
         document.querySelector('.msg').textContent="Enter name";
+        const email_msg=document.querySelector('.mail_error')
+        email_msg.style.display="block";
     }
     else if(!b){
         document.querySelector('.msg').textContent="enter pasword";
+        const pass_msg=document.querySelector('.pass_error')
+        pass_msg.style.display="block";
     }
     if(a && b){
         document.querySelector('.msg').textContent="loged in";
