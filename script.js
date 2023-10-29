f38fda60628b7f9d945314e34496e5729d11200f
roopesh-jsp <rupzkumar5@gmail.com>
2023-10-29 16:08:36 +0530

    register

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-'use strict';
-const email_msg=document.querySelector('.mail_error');
-const pass_msg=document.querySelector('.pass_error');
-const pass_input=document.querySelector('#password');
-const mail_input=document.querySelector('#name');
-document.querySelector('.btn').addEventListener('click',function(){
-    let a=document.querySelector('#name').value;
-    let b=document.querySelector('#password').value;
-    if(!a){
-        document.querySelector('.msg').textContent="Enter name";
-        mail_input.style.border="3px solid purple";
-        email_msg.style.display="block";
-    }
-    else if(!b){
-        document.querySelector('.msg').textContent="enter pasword";
-        email_msg.style.display="none";
-        mail_input.style.border="none";
-        pass_input.style.border="3px solid purple";
-        pass_msg.style.display="block";
-    }
-    if(a && b){
-        document.querySelector('.msg').textContent="loged in";
-    }
-    
-    
+"use strict";
+const email_msg = document.querySelector(".mail_error");
+const pass_msg = document.querySelector(".pass_error");
+const pass_input = document.querySelector("#password");
+const mail_input = document.querySelector("#name");
+document.querySelector(".btn").addEventListener("click", function () {
+  let a = document.querySelector("#name").value;
+  let b = document.querySelector("#password").value;
+  if (!a) {
+    document.querySelector(".msg").textContent = "Enter name";
+    mail_input.style.border = "3px solid purple";
+    email_msg.style.display = "block";
+  } else if (!b) {
+    document.querySelector(".msg").textContent = "enter pasword";
+    email_msg.style.display = "none";
+    mail_input.style.border = "none";
+    pass_input.style.border = "3px solid purple";
+    pass_msg.style.display = "block";
+  }
+  if (a && b) {
+    document.querySelector(".msg").textContent = "loged in";
+  }
+});
+const user = document.querySelector(".user");
+const pass = document.querySelector(".pass");
+const sign = document.querySelector(".sign");
+sign.addEventListener("click", function () {
+  user.textContent = "Name";
+  pass.textContent = "Gmail";
+  sign.textContent = "login";
 });
